dae22012f5c2314112b1736d31266939dc0a5583
Leo Durrant <leodurrant@microsoft.com>
2025-10-07 14:52:35 +0100

    fix: correct ambiguity response

--- Tools/JourneyPlanner.cs
+++ Tools/JourneyPlanner.cs
@@ -13,9 +13,15 @@ namespace MCPServerDemo.Tools
             [Description("Origin of the journey. Can be expressed as  \"lat,long\", a UK postcode, a Naptan (StopPoint) id, an ICS StopId, or a free-text string (will cause disambiguation unless it exactly matches a point of interest name).")] string fromLocation,
             [Description("Destination of the journey. Can be WGS84 coordinates expressed as \"lat,long\", a UK postcode, a Naptan (StopPoint) id, an ICS StopId, or a free-text string (will cause disambiguation unless it exactly matches a point of interest name).")] string toLocation,
             [Description("(Optional) Travel through point on the journey. Can be WGS84 coordinates expressed as \"lat,long\", a UK postcode, a Naptan (StopPoint) id, an ICS StopId, or a free-text string (will cause disambiguation unless it exactly matches a point of interest name).")] string via = "",
-            [Description("Does the journey cover stops outside London? eg. \"nationalSearch=true\"")] bool nationalSearch = false)
+            [Description("(Optional) Does the journey cover stops outside London zones? eg. \"nationalSearch=true\"")] bool nationalSearch = false,
+            [Description("(Optional) The date must be in yyyyMMdd format")] string date="",
+            [Description("(Optional) The time must be in HHmm format")] string time="",
+            [Description("(Optional) Does the time given relate to arrival or leaving time? Possible options: \"departing\" | \"arriving\" (Default: departing)")] string timeIs="departing",
+            [Description("(Optional) The journey preference eg possible options: \"leastinterchange\" | \"leasttime\" | \"leastwalking\" (Default: leastinterchange)")] string journeyPreference="leastinterchange",
+            [Description("(Optional) The mode must be a comma separated list of modes. eg possible options (and default): \"bus,overground,national-rail,tube,coach,dlr,cable-car,tram,river-bus,walking,cycle\"")] string mode= "bus,overground,national-rail,tube,coach,dlr,cable-car,tram,river-bus,walking,cycle",
+            [Description("(Optional) A boolean to make Journey Planner return alternative routes. Alternative routes are calculated by removing one or more lines included in the fastest route and re-calculating. By default, these journeys will not be returned. (Default: false)")] bool includeAlternativeRoutes=false)
         {
-            var url = $"/Journey/JourneyResults/{fromLocation}/to/{toLocation}?nationalSearch={nationalSearch}" + (via != "" ? $"&via={via}" : "");
+            var url = $"/Journey/JourneyResults/{fromLocation}/to/{toLocation}?nationalSearch={nationalSearch}&timeIs={timeIs}&journeyPreference={journeyPreference}&mode={mode}&includeAlternativeRoutes={includeAlternativeRoutes}" + (via != "" ? $"&via={via}" : "") + (date != "" ? $"&date={date}" : "") + (time != "" ? $"&time={time}" : "");
             using var response = await client.GetAsync(url);
 
             JsonDocument jDoc = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
@@ -24,7 +30,7 @@ namespace MCPServerDemo.Tools
             if (response.StatusCode == System.Net.HttpStatusCode.MultipleChoices) {
                 StringBuilder output = new StringBuilder();
 
-                output.AppendLine("Location ambiguity response; Re-run this tool with your input clarified on which point of interest you are trying to plan to or from.");
+                output.AppendLine("Location ambiguity response; Re-run this tool with your input clarified on which point of interest you are trying to plan to go or from or via.");
 
                 if (jElem.TryGetProperty("fromLocationDisambiguation", out var fromLocationDis))
                 {
@@ -33,7 +39,12 @@ namespace MCPServerDemo.Tools
 
                 if (jElem.TryGetProperty("toLocationDisambiguation", out var toLocationDis))
                 {
-                    output.AppendLine(ListDisambiguationOptions("From", toLocationDis));
+                    output.AppendLine(ListDisambiguationOptions("To", toLocationDis));
+                }
+
+                if (jElem.TryGetProperty("viaLocationDisambiguation", out var viaLocationDis))
+                {
+                    output.AppendLine(ListDisambiguationOptions("Via", viaLocationDis));
                 }
 
                 return output.ToString();
